33e1ae3ec8f59af4198035a9cf62b30c3156272c
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2023-03-27 19:04:38 +0800

    Basic express server setup for project

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Task Manager app');
+});
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}....`));
