98ec3ff85d7df7aa714c0450f97225909cc1725f
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2023-03-27 21:17:25 +0800

    setup get, post, update & delete task routes and controllers

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 //importing controllers
-const {getAllTasks} = require('../controllers/tasks');
+const {getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks');
 
 //routes
-router.route('/').get(getAllTasks);
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 
 module.exports = router;
