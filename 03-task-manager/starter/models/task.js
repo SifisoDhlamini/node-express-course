859b1550ebb61fcd8daae9a2f725b3ba9afdf626
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2023-03-31 22:16:08 +0800

    fixed validation for create tasks

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose');
 
 
 const TaskSchema = new mongoose.Schema({
-   name:String,
-   completed: Boolean
+   name:{
+         type: String,
+         required: [true, "Must provide a name for the task"],
+         trim: true,
+         minlength: [3, "Name must be at least 3 characters long"],
+         maxlength: [20, "Name must be less than 20 characters long"]
+   },
+   completed: {
+         type: Boolean,
+         default: false
+   }
 });
 
 const Task = mongoose.model("Task", TaskSchema);
