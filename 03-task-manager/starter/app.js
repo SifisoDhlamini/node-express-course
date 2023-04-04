40e2205f911d11fb50bbb00947338451de01134a
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2023-04-05 00:33:31 +0800

    removed useles require in app.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-const { populate } = require('./models/task');
 require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorHandler = require("./middleware/error-handler");
